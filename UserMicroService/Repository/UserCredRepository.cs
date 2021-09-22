ab68a0bde78ff588f8f6c8e0e7bb694db3ef9ac9
kousiksen09 <1704189@kiit.ac.in>
2021-09-22 11:12:20 +0530

    Modify | Change Ocelot Gateway. Add | LogIn API and implemet log4net.

---
+++ UserMicroService/Repository/UserCredRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserMicroService.DBContext;
+using UserMicroService.Models;
+
+namespace UserMicroService.Repository
+{
+    public class UserCredRepository : IUserCredRepository
+    {
+        private readonly UserContext _userContext;
+        public UserCredRepository(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+        public UserCred GetUserCredById(int userid)
+        {
+            return _userContext.UserCreds.Find(userid);
+        }
+
+        public IEnumerable<UserCred> GetUserCreds()
+        {
+            return _userContext.UserCreds.ToList();
+        }
+    }
+}
