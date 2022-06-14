77b1cd3db5f50e4ab1ddbf52908c9e4a5c8e2607
kousiksen09 <1704189@kiit.ac.in>
2022-06-14 09:41:09 +0530

    Remove | Mobile static files

--- Retail_Bank_UI/wwwroot/lib/Mobile/examples/gen/js/hiddenInput.js
+++
@@ -1,5 +0,0 @@
-var input = document.querySelector("#phone");
-window.intlTelInput(input, {
-  hiddenInput: "full_phone",
-  utilsScript: "../../build/js/utils.js?1613236686837" // just for formatting/placeholders etc
-});
